bfe166ed3669a264d519737112c347bb1a02d136
RedSavant <jjumper999@gmail.com>
2025-12-18 07:50:04 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -89,13 +89,6 @@ client.on(Events.InteractionCreate, async interaction => {
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-	readyClient.user.setPresence({
-        activities: [{
-            type: 4,
-            state: "Powered by UnderScape"
-        }],
-        status: "online"
-    });
 });
 
 client.login(process.env.TOKEN)
